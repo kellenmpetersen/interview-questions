3a775a6e3935725c92925ac68d7f67b6533a2599
Kellen Petersen <kellen.petersen@my.wheaton.edu>
2023-10-19 14:40:13 -0700

    167. Two Sum II - Input Array Is Sorted

---
+++ leetcode-medium/167-two-sum-ii-input-array-is-sorted.java
@@ -0,0 +1,20 @@
+//  LeetCode - 125. Valid Palindrome
+//  https://leetcode.com/problems/valid-palindrome
+//  time to solve: 5:22 (Oct 19 2023)
+//  concept: NeetCode (2/5) Two Pointers
+//  TODO:   read the problem and examples carefully. I didn't return
+//          the correct indices initally. 
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int num1 = 0, num2 = numbers.length-1;
+        while(numbers[num1] + numbers[num2] != target){
+            if(target > numbers[num1] + numbers[num2]){
+                num1++;
+            } else if(target < numbers[num1] + numbers[num2]){
+                num2--;
+            }
+        }
+        return new int[]{++num1,++num2};
+    }
+}
